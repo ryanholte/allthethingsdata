0b86656c9ab92f4d5da925bf7d71ff02ed4ccf72
ryanholte <ryan.holte.1@my.bismarckstate.edu>
2018-09-29 12:25:09 -0500

    [CHORE] adds ability to use custom routes

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = {
   route: require('./lib/route'),
-}
+  io: require('./lib/io'),
+  db: require('./lib/db/index'),
+  setup(connection, debug) {
+    require('./lib/db/index').seed(connection, debug);
+  }
+};
